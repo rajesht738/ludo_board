25abc551ebc16ad63e755b8c988b5bf6d9167b1b
Rajesh Tiwari <tiwarirj73@gmail.com>
2024-01-02 14:39:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Board from "./board";
+const App = () =>
+ (
+  <Board/>
+ 
+ );
+ReactDOM.render(<App />, document.getElementById("app"));
